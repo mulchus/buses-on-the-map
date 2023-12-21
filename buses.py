929f777a5f0f98d3bcf7c273c5f320fd434592db
mulchus <mulch@list.ru>
2023-12-21 23:59:02 +0300

    Added send and receive fake bus.

--- buses.py
+++ buses.py
@@ -4,7 +4,6 @@ import json
 def get_bus():
     with open('bus_routers/156.json', encoding='utf-8') as f:
         bus = json.load(f)
-        print(len(bus))
         return bus
 
 
--- server.py
+++ server.py
@@ -7,29 +7,31 @@ from trio_websocket import serve_websocket, ConnectionClosed
 
 
 async def server(request):
-    bus = buses.get_bus()
+    # bus = buses.get_bus()
     ws = await request.accept()
-    
-    buses_to_map = {
-        'msgType': "Buses",
-        'buses': [
-            {'busId': 'аXXXик', 'lat': 0.0, 'lng': 0.0, 'route': bus['name']},
-        ]
-    }
+    #
+    # buses_to_map = {
+    #     'msgType': "Buses",
+    #     'buses': [
+    #         {'busId': 'аXXXик', 'lat': 0.0, 'lng': 0.0, 'route': bus['name']},
+    #     ]
+    # }
     
     while True:
-        bus_coords = buses.get_bus_coords(bus)
+        # bus_coords = buses.get_bus_coords(bus)
         try:
-            for bus_coord in bus_coords:
-                buses_to_map['buses'][0]['lat'] = bus_coord[0]
-                buses_to_map['buses'][0]['lng'] = bus_coord[1]
-                await ws.send_message(json.dumps(buses_to_map))
-                await trio.sleep(1)
+            # for bus_coord in bus_coords:
+            message = await ws.get_message()
+            print(message)
+                # buses_to_map['buses'][0]['lat'] = bus_coord[0]
+                # buses_to_map['buses'][0]['lng'] = bus_coord[1]
+                # await ws.send_message(json.dumps(buses_to_map))
+            await trio.sleep(.1)
         except ConnectionClosed:
             break
 
 
 async def main():
-    await serve_websocket(server, '127.0.0.1', 8000, ssl_context=None)
+    await serve_websocket(server, '127.0.0.1', 8080, ssl_context=None)
 
 trio.run(partial(main))
