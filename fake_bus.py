c6a99cd84aef9b87965e8ade9377f7616bff878c
mulchus <mulch@list.ru>
2023-12-22 12:51:36 +0300

    Added sending coordinates of some buses.

--- fake_bus.py
+++ fake_bus.py
@@ -1,32 +1,32 @@
 import trio
 import json
-import buses
 
+from load_routes import load_routes
 from sys import stderr
 from trio_websocket import open_websocket_url
 
 
-async def main():
-    bus = buses.get_bus()
-    fake_bus = {'busId': 'аXXXик', 'lat': 0.0, 'lng': 0.0, 'route': bus['name']}
-    # buses_to_map = {
-    #     'msgType': "Buses",
-    #     'buses': [
-    #         {'busId': 'аXXXик', 'lat': 0.0, 'lng': 0.0, 'route': bus['name']},
-    #     ]
-    # }
+async def run_bus(url, bus_id, route):
     try:
-        bus_coords = buses.get_bus_coords(bus)
-        async with open_websocket_url('ws://127.0.0.1:8080') as ws:  # ws://127.0.0.1:8080/ws
-            for bus_coord in bus_coords:
-                fake_bus['lat'] = bus_coord[0]
-                fake_bus['lng'] = bus_coord[1]
-                await ws.send_message(json.dumps(fake_bus, ensure_ascii=False))
-                await trio.sleep(1)
-            # await ws.send_message('hello world!')
-            # message = await ws.get_message()
-            # print('Received message: %s' % message)
+        async with open_websocket_url(url) as ws:
+            while True:
+                fake_bus = {'busId': bus_id, 'lat': 0.0, 'lng': 0.0, 'route': route['name']}
+                for bus_coord in route['coordinates']:
+                    fake_bus['lat'] = bus_coord[0]
+                    fake_bus['lng'] = bus_coord[1]
+                    await ws.send_message(json.dumps(fake_bus, ensure_ascii=False))
+                    await trio.sleep(1)
     except OSError as ose:
         print('Connection attempt failed: %s' % ose, file=stderr)
+
+
+async def main():
+    async with trio.open_nursery() as nursery:
+        for num, route in enumerate(load_routes()):
+            print(num, route['name'])
+            nursery.start_soon(run_bus, 'ws://127.0.0.1:8080', route['name'], route)
+            if num > 2:
+                return
+
 
 trio.run(main)
--- server.py
+++ server.py
@@ -1,32 +1,15 @@
 import trio
-import json
-import buses
 
 from functools import partial
 from trio_websocket import serve_websocket, ConnectionClosed
 
 
 async def server(request):
-    # bus = buses.get_bus()
     ws = await request.accept()
-    #
-    # buses_to_map = {
-    #     'msgType': "Buses",
-    #     'buses': [
-    #         {'busId': 'аXXXик', 'lat': 0.0, 'lng': 0.0, 'route': bus['name']},
-    #     ]
-    # }
-    
     while True:
-        # bus_coords = buses.get_bus_coords(bus)
         try:
-            # for bus_coord in bus_coords:
             message = await ws.get_message()
             print(message)
-                # buses_to_map['buses'][0]['lat'] = bus_coord[0]
-                # buses_to_map['buses'][0]['lng'] = bus_coord[1]
-                # await ws.send_message(json.dumps(buses_to_map))
-            # await trio.sleep(1)
         except ConnectionClosed:
             break
 
