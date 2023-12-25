e84ec08a170ffc008b1d61836a545a1796c77717
mulchus <mulch@list.ru>
2023-12-25 21:15:44 +0300

    Upgrade sequence in main.

--- fake_bus.py
+++ fake_bus.py
@@ -18,7 +18,7 @@ async def run_bus(bus_id, route, send_channel):
                 fake_bus['lng'] = bus_coord[1]
                 # print(fake_bus)
                 await send_channel.send(json.dumps(fake_bus, ensure_ascii=False))
-                # await trio.sleep(1)
+                await trio.sleep(1)
 
 
 def generate_bus_id(route_id, bus_index):
@@ -37,47 +37,34 @@ async def send_updates(server_address, receive_channel):
             print('Connection attempt failed: %s' % ose, file=stderr)
 
 
-# async def produced_buses(send_channel):
-    # Producer sends 3 messages
-    # for i in range(3):
-        # The producer sends using 'await send_channel.send(...)'
-        # await send_channel.send(f"message {i}")
-
-
-# async def consumer(receive_channel):
-#     # The consumer uses an 'async for' loop to receive the values:
-#     async for value in receive_channel:
-#         print(f"got value {value!r}")
-
-
 async def main():
-    async with trio.open_nursery() as nursery:
-        send_channel, receive_channel = trio.open_memory_channel(0)
-        # async with send_channel, receive_channel:
-        nursery.start_soon(send_updates, 'ws://127.0.0.1:8080', receive_channel)
-
-        all_buses_count = 0
-        for num, route in enumerate(load_routes()):
-            buses_count = len(route['stations']) // 4
-            if not buses_count:
-                buses_count = 1
-            all_buses_count += buses_count
-            print(route['name'], buses_count)
-            for bus_index in range(buses_count):
-                route_copy = route.copy()
-                bus_id = generate_bus_id(route_copy['name'], bus_index)
-                route_len = len(route_copy['coordinates'])
-                route_separation = random.randint(0, route_len)
-                new_route = list(itertools.islice(route_copy['coordinates'], route_separation, route_len))
-                new_route_end = list(itertools.islice(route_copy['coordinates'], 0, route_separation))
-                new_route.extend(new_route_end)
-                route_copy['coordinates'] = new_route
-                random.choice([nursery.start_soon(run_bus, bus_id, route_copy, send_channel.clone()),
-                               nursery.start_soon(run_bus, bus_id, route_copy, send_channel.clone()),
-                               nursery.start_soon(run_bus, bus_id, route_copy, send_channel.clone())])
-            # if num > 0:
-            #     return
-        print(all_buses_count)
+    send_channel, receive_channel = trio.open_memory_channel(0)
+    async with send_channel, receive_channel:
+        async with trio.open_nursery() as nursery:
+            nursery.start_soon(send_updates, 'ws://127.0.0.1:8080', receive_channel)
+    
+            all_buses_count = 0
+            for num, route in enumerate(load_routes()):
+                buses_count = len(route['stations']) // 5
+                if not buses_count:
+                    buses_count = 1
+                all_buses_count += buses_count
+                print(route['name'], buses_count)
+                for bus_index in range(buses_count):
+                    route_copy = route.copy()
+                    bus_id = generate_bus_id(route_copy['name'], bus_index)
+                    route_len = len(route_copy['coordinates'])
+                    route_separation = random.randint(0, route_len)
+                    new_route = list(itertools.islice(route_copy['coordinates'], route_separation, route_len))
+                    new_route_end = list(itertools.islice(route_copy['coordinates'], 0, route_separation))
+                    new_route.extend(new_route_end)
+                    route_copy['coordinates'] = new_route
+                    random.choice([nursery.start_soon(run_bus, bus_id, route_copy, send_channel.clone()),
+                                   nursery.start_soon(run_bus, bus_id, route_copy, send_channel.clone()),
+                                   nursery.start_soon(run_bus, bus_id, route_copy, send_channel.clone())])
+                if num > 0:
+                    return
+            print(all_buses_count)
 
 
 trio.run(main)
