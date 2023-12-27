0a1a4c996dcec389dad564571815519e4f743c37
mulchus <mulch@list.ru>
2023-12-28 00:55:35 +0300

    Added bus filter on coordinates.

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import trio
 import json
+import logging
 
 from functools import partial
 from trio_websocket import serve_websocket, ConnectionClosed
@@ -7,6 +8,12 @@ from contextlib import suppress
 
 
 buses = {}
+bounds = {'south_lat': 0, 'north_lat': 0, 'west_lng': 0, 'east_lng': 0}
+logger = logging.getLogger('logger')
+
+
+def is_inside(lat, lng):
+    return bounds['south_lat'] <= lat <= bounds['north_lat'] and bounds['west_lng'] <= lng <= bounds['east_lng']
 
 
 async def server(request):
@@ -15,6 +22,10 @@ async def server(request):
     while True:
         try:
             bus = dict(json.loads(await ws.get_message()))
+            if not is_inside(bus['lat'], bus['lng']):
+                if bus['busId'] in buses:
+                    buses.pop(bus['busId'])
+                continue
             if bus['busId'] not in buses:
                 buses[bus['busId']] = bus
             else:
@@ -23,25 +34,40 @@ async def server(request):
             break
 
 
-async def talk_to_browser(request):
-    ws = await request.accept()
+async def listen_browser(ws):
+    global bounds
+    while True:
+        try:
+            bounds = json.loads(await ws.get_message())['data']
+        except ConnectionClosed:
+            break
+
+
+async def send_to_browser(ws):
     while True:
         try:
             message = {
-              "msgType": "Buses",
-              "buses": list(buses.values()),
+                "msgType": "Buses",
+                "buses": list(buses.values()),
             }
-            # print(message)
             await ws.send_message(json.dumps(message))
             await trio.sleep(.1)
         except ConnectionClosed:
             break
 
 
+async def talk_with_browser(request):
+    ws = await request.accept()
+    async with trio.open_nursery() as nursery:
+        nursery.start_soon(listen_browser, ws)
+        nursery.start_soon(send_to_browser, ws)
+    
+
 async def main():
+    logger.setLevel(logging.DEBUG)
     async with trio.open_nursery() as nursery:
         nursery.start_soon(partial(serve_websocket, server, '127.0.0.1', 8080, ssl_context=None))
-        nursery.start_soon(partial(serve_websocket, talk_to_browser, '127.0.0.1', 8000, ssl_context=None))
+        nursery.start_soon(partial(serve_websocket, talk_with_browser, '127.0.0.1', 8000, ssl_context=None))
 
 
 with suppress(KeyboardInterrupt):
